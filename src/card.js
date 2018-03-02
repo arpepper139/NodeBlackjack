daf9deefaa8c4b19cbca2c45022775f465321082
Andrew <apepperanderson@gmail.com>
2018-03-02 11:09:44 -0500

    work

--- src/card.js
+++ src/card.js
@@ -1,18 +1,20 @@
 class Card {
   constructor(rank, suit) {
-    this.rank = rank,
-    this.suit = suit
+    this.rank = rank;
+    this.suit = suit;
   }
 
   initialValue() {
     if (this.rank === 'A') {
-      return 1
+      return 1;
     }
     else if (this.rank === "K" || this.rank === "Q" || this.rank == "J") {
-      return 10
+      return 10;
     }
     else {
-      return +(this.rank)
+      return +(this.rank);
     }
   }
 }
+
+module.exports = Card;
