f01166ce16e3096e33dbb602d9b6f3026756be05
Andrew <apepperanderson@gmail.com>
2018-03-02 23:07:01 -0500

    working app

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Node Blackjack
 
-A basic command line blackjack application built in Javascript, utilizing Node.js. Currently in development.
+A basic command line blackjack application built in Javascript, utilizing Node.js.
+My first Node-based CLI. Currently in development.
 
 To play enter `node app/runner.js` in the command line.
--- app/runner.js
+++ app/runner.js
@@ -5,37 +5,66 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const Card = require('./models/card');
 const Hand = require('./models/hand');
 const Deck = require('./models/deck');
-
-const welcomeMessage = 'Welcome to Blackjack!\n'
-const bustMessage = 'Bust! You lose.'
 
 initialHandInfo = (player, hand) => {
   let infoString = '';
   infoString += `${player} was dealt ${hand.cards[0].rank}${hand.cards[0].suit}\n`;
   infoString += `${player} was dealt ${hand.cards[1].rank}${hand.cards[1].suit}\n`;
-  infoString += `${player} Score: ${hand.value()}\n`;
+  infoString += `${player}'s Score: ${hand.value()}\n`;
   return infoString;
 };
 
-hitOrStand = () => {
+determineWinner = (playerHand, computerHand) => {
+  let playerScore = playerHand.value();
+  let computerScore = computerHand.value();
+  if (playerScore > computerScore || computerScore > 21) {
+    console.log('Congrats! You win!')
+  }
+  else if (playerScore === computerScore) {
+    console.log('Push!')
+  }
+  else {
+    console.log('Dealer Wins.')
+  }
+};
+
+playBlackjack = (playerHand, computerHand) => {
   rl.question('Hit or stand? (h/s)> ', (answer) => {
     choice = answer.trim().toLowerCase();
-    if (choice === 'h' || choice === 's') {
+    if (choice === 'h') {
+      let newPlayerCard = deck.deal(1)[0];
+      playerHand.hit(newPlayerCard);
+      if (playerHand.value() <= 21) {
+        console.log(`You were dealt ${newPlayerCard.rank}${newPlayerCard.suit}`);
+        console.log(`Score: ${playerHand.value()}`);
+        playBlackjack(playerHand, computerHand);
+      }
+      else {
+        console.log(`You were dealt ${newPlayerCard.rank}${newPlayerCard.suit}`);
+        console.log(`Score: ${playerHand.value()}`);
+        console.log('Bust: You lose.');
+        rl.close();
+      }
+    }
+    else if (choice === 's') {
+      console.log(initialHandInfo('Dealer', computerHand));
+      while (computerHand.value() < 17) {
+        let newComputerCard = deck.deal(1)[0];
+        computerHand.hit(newComputerCard);
+        console.log(`Dealer was dealt ${newComputerCard.rank}${newComputerCard.suit}`);
+        console.log(`Dealer Score: ${computerHand.value()}`);
+      }
+      determineWinner(playerHand, computerHand);
       rl.close();
-      return choice;
     }
     else {
-      console.log('Sorry! You can only hit or stand.\n')
-      hitOrStand();
+      console.log('Sorry! You can only hit or stand.\n');
+      playBlackjack(playerHand, computerHand);
     }
   });
 };
-
-
-
 
 //Game
 
@@ -45,6 +74,10 @@ deck.shuffle();
 const playerHand = new Hand(deck.deal(2));
 const computerHand = new Hand(deck.deal(2));
 
-console.log(welcomeMessage);
-console.log(initialHandInfo('Player', playerHand));
-hitOrStand();
+rl.question('What is your name?> ', (answer) => {
+  let name = answer.trim();
+  console.log(`Welcome ${name}! Let's play blackjack!`);
+  console.log(initialHandInfo(name, playerHand));
+
+  playBlackjack(playerHand, computerHand);
+});
